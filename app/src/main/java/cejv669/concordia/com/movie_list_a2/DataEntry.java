9256cc444f43b233e316e82227969c928e6d619f
bghassemieh <bghassemieh@yahoo.com>
2019-03-12 01:40:49 -0400

    Initial commit

---
+++ app/src/main/java/cejv669/concordia/com/movie_list_a2/DataEntry.java
@@ -0,0 +1,13 @@
+package cejv669.concordia.com.movie_list_a2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DataEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_entry_layout);
+    }
+}
